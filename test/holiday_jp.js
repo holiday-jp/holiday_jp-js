dcf18b0fb5b74d67a808d6e2788003bd6718483f
k1LoW <k1lowxb@gmail.com>
2014-11-17 22:34:25 +0900

    Add testcase

--- test/holiday_jp.js
+++ test/holiday_jp.js
@@ -3,23 +3,27 @@ var files = fs.readdirSync('lib');
 var expect = require('chai').expect;
 var holiday_jp = require('./../release/holiday_jp'); // test release build
 
-describe('holiday_jp', function(){
-  it('should have New Year\'s Day(2009-01-01) between 2009-01-01 and 2009-01-31', function(){
-    var holidays = holiday_jp.between(new Date('2009-01-01'), new Date('2009-01-31'));
-    var new_year_day = holidays[0];
-    expect(new_year_day['date'] == new Date('2009-01-01'));
-  });
-  
+describe('holiday_jp', function(){  
   it('should have valid version', function(){
     expect(holiday_jp.VERSION).to.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
   });
 
-  it('should be holiday 2011-09-19', function(){
-    expect(holiday_jp.isHoliday(new Date('2011-09-19'))).to.eq(true);
+  it('.between should have New Year\'s Day(2009-01-01) between 2009-01-01 and 2009-01-31', function(){
+    var holidays = holiday_jp.between(new Date('2009-01-01'), new Date('2009-01-31'));
+    var new_year_day = holidays[0];
+    expect(new_year_day['date'] == new Date('2009-01-01'));
   });
 
-  it('should not be holiday 2011-09-18', function(){
+  it('.isHoliday should be holiday returns date is holiday or not', function(){
+    expect(holiday_jp.isHoliday(new Date('2011-09-19'))).to.eq(true);
     expect(holiday_jp.isHoliday(new Date('2011-09-18'))).to.not.eq(true);
+  });
+
+  it('should be Mountain Day from 2016', function(){
+    expect(holiday_jp.isHoliday(new Date('2015-08-11'))).to.not.eq(true);
+    for (year = 2016; year <= 2050; year++) {
+      expect(holiday_jp.isHoliday(new Date(year + '-08-11'))).to.eq(true);
+    }
   });
 });
 
