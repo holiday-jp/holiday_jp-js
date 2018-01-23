4d04515fba11d7d9352e3a7dca79dc068639abb9
k1LoW <k1lowxb@gmail.com>
2018-01-23 09:43:58 +0900

    Fix holidays

--- scripts/generate.js
+++ scripts/generate.js
@@ -6,10 +6,10 @@ var fs = require('fs')
 var holidays = yaml.safeLoad(fs.readFileSync(__dirname + '/../holiday_jp/holidays_detailed.yml', 'utf8'));
 var timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
 var jsfile = [util.format("// Generated from holidays.yml at %s;", timestamp)];
-jsfile.push('exports.holidays = {};');
+jsfile.push('var holidays = {};');
 
 Object.keys(holidays).forEach(function (date) {
-  jsfile.push("exports.holidays['" + moment(new Date(date)).format('YYYY-MM-DD') + "'] = {");
+  jsfile.push("holidays['" + moment(new Date(date)).format('YYYY-MM-DD') + "'] = {");
   jsfile.push("  'date': '" + moment(new Date(date)).format('YYYY-MM-DD') + "',");
   jsfile.push("  'week': '" + holidays[date]['week'] +"',");
   jsfile.push("  'week_en': '" + holidays[date]['week_en'] +"',");
@@ -17,5 +17,6 @@ Object.keys(holidays).forEach(function (date) {
   jsfile.push("  'name_en': \"" + holidays[date]['name_en'] +"\"");
   jsfile.push('};');
 });
+jsfile.push('module.exports = holidays;');
 
 fs.writeFileSync(__dirname + '/../lib/holidays.js', jsfile.join("\n"));
