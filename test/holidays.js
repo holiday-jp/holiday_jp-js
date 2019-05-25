e972018c07f1b7b133e659dcd75b8e4c4da11662
k1LoW <k1lowxb@gmail.com>
2019-05-25 17:43:14 +0900

    Update holiday-jp/holiday_jp dataset

--- test/holidays.js
+++ test/holidays.js
@@ -1,23 +1,25 @@
-var fs = require('fs');
-var files = fs.readdirSync('lib');
 var expect = require('chai').expect;
+var request = require('request');
 var yaml = require('js-yaml');
 var holidays = require('./../lib/holidays');
 
-var TESTSET_DIR = __dirname + '/../holiday_jp/';
+var HOLIDAYS_DETAIL_URL = 'https://raw.githubusercontent.com/holiday-jp/holiday_jp/master/holidays_detailed.yml';
 
 describe('holidays', function(){
-  it('holidays should be holiday all holidays_detailed.yml', function(){
-    var testset = yaml.safeLoad(fs.readFileSync(TESTSET_DIR + 'holidays_detailed.yml', 'utf8'));
-    function format(date) {
-      var year = date.getFullYear();
-      var month = ('0' + (date.getMonth() + 1)).slice(-2);
-      var day = ('0' + (date.getDate())).slice(-2);
-      return (year + '-' + month + '-' + day);
-    }
-    Object.keys(testset).forEach(function (key) {
-      var d = format(new Date(key));
-      expect(holidays[d].name).to.eq(testset[key].name);
+  it('holidays should be holiday all holidays_detailed.yml', function(done){
+    request(HOLIDAYS_DETAIL_URL, function (error, response, body) {
+      var testset = yaml.safeLoad(body);
+      function format(date) {
+        var year = date.getFullYear();
+        var month = ('0' + (date.getMonth() + 1)).slice(-2);
+        var day = ('0' + (date.getDate())).slice(-2);
+        return (year + '-' + month + '-' + day);
+      }
+      Object.keys(testset).forEach(function (key) {
+        var d = format(new Date(key));
+        expect(holidays[d].name).to.eq(testset[key].name);
+      });
+      done();
     });
   });
 });
