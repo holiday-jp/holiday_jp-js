21f45c57cabc6a69eea9b1814856ad9d3de1967a
camelmasa <camelmasa@gmail.com>
2022-07-28 22:36:30 +0900

    formatter

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,13 +1,13 @@
-const fs = require('fs')
-  , util = require('util')
-  , yaml = require('js-yaml')
-  , moment = require('moment');
+const fs = require('fs'),
+  util = require('util'),
+  yaml = require('js-yaml'),
+  moment = require('moment');
 
 const writeFile = (file_name, holidays) => {
   const timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
-  const jsfile = [util.format("// Generated from holidays.yml at %s;", timestamp)];
+  const jsfile = [util.format('// Generated from holidays.yml at %s;', timestamp)];
   jsfile.push('var holidays = {};');
-  
+
   for (const date of Object.keys(holidays)) {
     jsfile.push(`holidays['${moment(new Date(date)).format('YYYY-MM-DD')}'] = {`);
     jsfile.push(`  'date': '${moment(new Date(date)).format('YYYY-MM-DD')}',`);
@@ -19,19 +19,19 @@ const writeFile = (file_name, holidays) => {
   }
   jsfile.push('module.exports = holidays;');
   jsfile.push('');
-  
-  fs.writeFileSync(file_name, jsfile.join("\n"));
-}
+
+  fs.writeFileSync(file_name, jsfile.join('\n'));
+};
 
 const holidays = yaml.safeLoad(fs.readFileSync(__dirname + '/../holiday_jp/holidays_detailed.yml', 'utf8'));
-writeFile(`${__dirname}/../lib/holidays.js`, holidays)
+writeFile(`${__dirname}/../lib/holidays.js`, holidays);
 
 const holidays_every_year = Object.keys(holidays).reduce((holidays_every_year, date) => {
-  const year = moment(new Date(date)).format('YYYY')
-  const holidays_per_year = { ...holidays_every_year[year], [date]: holidays[date] }
-  return { ...holidays_every_year, [year]: holidays_per_year }
-}, {})
+  const year = moment(new Date(date)).format('YYYY');
+  const holidays_per_year = { ...holidays_every_year[year], [date]: holidays[date] };
+  return { ...holidays_every_year, [year]: holidays_per_year };
+}, {});
 
 for (const year of Object.keys(holidays_every_year)) {
-  writeFile(`${__dirname}/../lib/holidays_every_year/${year}.js`, holidays_every_year[year])
+  writeFile(`${__dirname}/../lib/holidays_every_year/${year}.js`, holidays_every_year[year]);
 }
--- test/no_autoload_holidays.js
+++ test/no_autoload_holidays.js
@@ -4,45 +4,49 @@ var holidays_1970 = require('../lib/holidays_every_year/1970');
 var holidays_1971 = require('../lib/holidays_every_year/1971');
 var holidays_1972 = require('../lib/holidays_every_year/1972');
 
-describe('no_autoload', function(){
-  it('should have valid version', function(){
+describe('no_autoload', function () {
+  it('should have valid version', function () {
     expect(holiday_jp.VERSION).to.match(/^[0-9]+\.[0-9]+\.[0-9]+$/);
   });
 
-  it('.isContainHolidays returns true if holiday dataset contains specified date', function(){
+  it('.isContainHolidays returns true if holiday dataset contains specified date', function () {
     holiday_jp.setHolidays([holidays_1970, holidays_1971, holidays_1972]);
-    expect(holiday_jp.isContainHolidays(new Date('1970-01-01'))).to.eq(true)
+    expect(holiday_jp.isContainHolidays(new Date('1970-01-01'))).to.eq(true);
   });
 
-  it('.isContainHolidays returns false if holiday dataset does not contain specified date', function(){
+  it('.isContainHolidays returns false if holiday dataset does not contain specified date', function () {
     holiday_jp.setHolidays([holidays_1970, holidays_1971, holidays_1972]);
-    expect(holiday_jp.isContainHolidays(new Date('1973-01-01'))).to.eq(false)
+    expect(holiday_jp.isContainHolidays(new Date('1973-01-01'))).to.eq(false);
   });
 
-  it('.setHolidays', function(){
+  it('.setHolidays', function () {
     holiday_jp.setHolidays([holidays_1970]);
-    expect(holiday_jp.holiday_years).to.deep.equal([1970])
-    expect(holiday_jp.holidays).to.not.eq([])
+    expect(holiday_jp.holiday_years).to.deep.equal([1970]);
+    expect(holiday_jp.holidays).to.not.eq([]);
   });
 
-  it('.between throw error if holiday dataset does not contain specified date', function(){
+  it('.between throw error if holiday dataset does not contain specified date', function () {
     holiday_jp.setHolidays([holidays_1970, holidays_1971, holidays_1972]);
-    expect(holiday_jp.between.bind(holiday_jp, new Date('1970-01-01'), new Date('1973-01-31'))).to.throw("Not contained in the holiday dataset")
+    expect(holiday_jp.between.bind(holiday_jp, new Date('1970-01-01'), new Date('1973-01-31'))).to.throw(
+      'Not contained in the holiday dataset'
+    );
   });
 
-  it('.between should be holiday', function(){
+  it('.between should be holiday', function () {
     holiday_jp.setHolidays([holidays_1970, holidays_1971, holidays_1972]);
     var holidays = holiday_jp.between(new Date('1970-01-01'), new Date('1970-01-31'));
     var new_year_day = holidays[0];
     expect(new_year_day['date']).to.deep.eq(new Date('1970-01-01'));
   });
 
-  it('.isHoliday throw error if holiday dataset does not contain specified date', function(){
+  it('.isHoliday throw error if holiday dataset does not contain specified date', function () {
     holiday_jp.setHolidays([holidays_1970, holidays_1971, holidays_1972]);
-    expect(holiday_jp.isHoliday.bind(holiday_jp, new Date('1973-01-31'))).to.throw("Not contained in the holiday dataset")
+    expect(holiday_jp.isHoliday.bind(holiday_jp, new Date('1973-01-31'))).to.throw(
+      'Not contained in the holiday dataset'
+    );
   });
 
-  it('.isHoliday should be holiday returns date is holiday or not', function(){
+  it('.isHoliday should be holiday returns date is holiday or not', function () {
     holiday_jp.setHolidays([holidays_1970, holidays_1971, holidays_1972]);
     expect(holiday_jp.isHoliday(new Date('1970-01-01'))).to.eq(true);
     expect(holiday_jp.isHoliday(new Date('1970-01-02'))).to.eq(false);
