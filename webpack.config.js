8c8b2b3249e401ac337166e291a244119ce8e1e3
Akira HIGUCHI <akira_higuchi@tailor.tech>
2025-05-27 15:00:27 +0900

    chore: remove uglifyjs use terser(webpack's default)

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,30 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const config = {
+const baseConfig = {
   mode: 'production',
   entry: path.join(__dirname, 'lib/holiday_jp.js'),
   output: {
-    filename: 'holiday_jp.js',
     path: path.join(__dirname, 'release'),
     library: 'holiday_jp',
     libraryTarget: 'umd',
     globalObject: 'typeof self !== \'undefined\' ? self : this'
   },
-  optimization: {
-    minimize: false
-  }
 };
 
-export default config;
+export default [{
+  ...baseConfig,
+  output: {
+    ...baseConfig.output,
+    filename: 'holiday_jp.js',
+  },
+  optimization: { minimize: false }
+}, {
+  ...baseConfig,
+  output: {
+    ...baseConfig.output,
+    filename: 'holiday_jp.min.js',
+  },
+  optimization: { minimize: true },
+  devtool: 'source-map',
+}];
